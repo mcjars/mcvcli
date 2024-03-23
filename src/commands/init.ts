d8f2b6f78dbc39c7c3d4b139ce85603842b346f4
0x7d8 <rjansengd@gmail.com>
2024-03-23 23:24:02 +0100

    fix some identation

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -11,91 +11,91 @@ export type Args = {
 }
 
 export default async function init(args: Args, profileName?: string) {
-  if (!fs.existsSync(args.directory)) {
-    fs.mkdirSync(args.directory)
-  }
+	if (!fs.existsSync(args.directory)) {
+		fs.mkdirSync(args.directory)
+	}
 
-  const previousCwd = process.cwd()
-  process.chdir(args.directory)
+	const previousCwd = process.cwd()
+	process.chdir(args.directory)
 
-  if (fs.existsSync('.mccli.json')) {
-    console.log('mccli is already initialized!')
-    console.log('use', chalk.cyan('mccli version'), 'to get the currently installed version')
+	if (fs.existsSync('.mccli.json')) {
+		console.log('mccli is already initialized!')
+		console.log('use', chalk.cyan('mccli version'), 'to get the currently installed version')
 
-    process.chdir(previousCwd)
-    process.exit(1)
-  }
+		process.chdir(previousCwd)
+		process.exit(1)
+	}
 
-  const files = fs.readdirSync('.')
-  
-  const { jarFile } = await enquirer.prompt<{
-    jarFile: string
-  }>({
-    type: 'select',
-    message: 'Server Jar File',
-    name: 'jarFile',
-    choices: [
-      'Install New (Jar)',
+	const files = fs.readdirSync('.')
+	
+	const { jarFile } = await enquirer.prompt<{
+		jarFile: string
+	}>({
+		type: 'select',
+		message: 'Server Jar File',
+		name: 'jarFile',
+		choices: [
+			'Install New (Jar)',
 			'Install New (Modpack)',
-      ...files.filter((file) => file.endsWith('.jar'))
-    ]
-  })
+			...files.filter((file) => file.endsWith('.jar'))
+		]
+	})
 
-  switch (jarFile) {
-    case "Install New (Jar)": {
-      const { type } = await enquirer.prompt<{
-        type: api.SupportedProject
-      }>({
-        type: 'select',
-        message: 'Server Type',
-        name: 'type',
-        choices: [...api.supportedProjects]
-      })
+	switch (jarFile) {
+		case "Install New (Jar)": {
+			const { type } = await enquirer.prompt<{
+				type: api.SupportedProject
+			}>({
+				type: 'select',
+				message: 'Server Type',
+				name: 'type',
+				choices: [...api.supportedProjects]
+			})
 
-      console.log('checking versions...')
+			console.log('checking versions...')
 
-      const versions = await api.versions(type),
-        { version } = await enquirer.prompt<{
-          version: string
-        }>({
-          type: 'autocomplete',
-          message: 'Server Version',
-          name: 'version',
-          choices: versions.reverse(),
-          // @ts-ignore
-          limit: 10
-        })
+			const versions = await api.versions(type),
+				{ version } = await enquirer.prompt<{
+					version: string
+				}>({
+					type: 'autocomplete',
+					message: 'Server Version',
+					name: 'version',
+					choices: versions.reverse(),
+					// @ts-ignore
+					limit: 10
+				})
 
-      console.log('checking latest build...')
+			console.log('checking latest build...')
 
-      const builds = await api.builds(type, version),
-        latest = builds[0]
+			const builds = await api.builds(type, version),
+				latest = builds[0]
 
-      const { ramMB } = await enquirer.prompt<{
-        ramMB: number
-      }>({
-        type: 'numeral',
-        message: 'Server RAM (MB)',
-        name: 'ramMB',
-        min: 1024,
-        initial: 4096
-      })
+			const { ramMB } = await enquirer.prompt<{
+				ramMB: number
+			}>({
+				type: 'numeral',
+				message: 'Server RAM (MB)',
+				name: 'ramMB',
+				min: 1024,
+				initial: 4096
+			})
 
-      const config = new Config({
+			const config = new Config({
 				configVersion: 2,
-        __README: 'This file is used to store the configuration for the mccli tool. Do not modify this file unless you know what you are doing.',
-        jarFile: 'server.jar',
-        profileName: profileName ?? 'default',
+				__README: 'This file is used to store the configuration for the mccli tool. Do not modify this file unless you know what you are doing.',
+				jarFile: 'server.jar',
+				profileName: profileName ?? 'default',
 				modpackSlug: null,
 				modpackVersion: null,
-        ramMB
-      })
+				ramMB
+			})
 
-      await api.install(latest.download, config)
-      config.write()
+			await api.install(latest.download, config)
+			config.write()
 
-      break
-    }
+			break
+		}
 
 		case "Install New (Modpack)": {
 			const { modpackSlug } = await enquirer.prompt<{
@@ -179,46 +179,46 @@ export default async function init(args: Args, profileName?: string) {
 			break
 		}
 
-    default: {
-      console.log('checking installed version...')
+		default: {
+			console.log('checking installed version...')
 
-      const version = await getJarVersion(path.resolve(jarFile))
+			const version = await getJarVersion(path.resolve(jarFile))
 
-      const { latestJar, latestMc } = await api.latest(version.type, version.minecraftVersion!)
+			const { latestJar, latestMc } = await api.latest(version.type, version.minecraftVersion!)
 
-      console.log('installed jar location:', chalk.cyan(jarFile))
-      console.log('installed jar version:')
-      console.log('  type:', chalk.cyan(version.type))
-      if (version.minecraftVersion) console.log('  minecraft version:', chalk.cyan(version.minecraftVersion), latestMc === version.minecraftVersion ? chalk.green('(latest)') : chalk.red('(outdated)'))
-      if (version.jarVersion) console.log('  jar version:', chalk.cyan(version.jarVersion), latestJar === version.jarVersion ? chalk.green('(latest)') : chalk.red('(outdated)'))
+			console.log('installed jar location:', chalk.cyan(jarFile))
+			console.log('installed jar version:')
+			console.log('  type:', chalk.cyan(version.type))
+			if (version.minecraftVersion) console.log('  minecraft version:', chalk.cyan(version.minecraftVersion), latestMc === version.minecraftVersion ? chalk.green('(latest)') : chalk.red('(outdated)'))
+			if (version.jarVersion) console.log('  jar version:', chalk.cyan(version.jarVersion), latestJar === version.jarVersion ? chalk.green('(latest)') : chalk.red('(outdated)'))
 
-      const { ramMB } = await enquirer.prompt<{
-        ramMB: number
-      }>({
-        type: 'numeral',
-        message: 'Server RAM (MB)',
-        name: 'ramMB',
-        min: 1024,
-        initial: 4096
-      })
+			const { ramMB } = await enquirer.prompt<{
+				ramMB: number
+			}>({
+				type: 'numeral',
+				message: 'Server RAM (MB)',
+				name: 'ramMB',
+				min: 1024,
+				initial: 4096
+			})
 
-      const config = new Config({
+			const config = new Config({
 				configVersion: 2,
-        __README: 'This file is used to store the configuration for the mccli tool. Do not modify this file unless you know what you are doing.',
-        jarFile,
-        profileName: profileName ?? 'default',
+				__README: 'This file is used to store the configuration for the mccli tool. Do not modify this file unless you know what you are doing.',
+				jarFile,
+				profileName: profileName ?? 'default',
 				modpackSlug: null,
 				modpackVersion: null,
-        ramMB
-      })
+				ramMB
+			})
 
-      config.write()
+			config.write()
 
-      break
-    }
-  }
+			break
+		}
+	}
 
 	if (!profileName) fs.mkdirSync(path.join('.mccli.profiles', 'default'), { recursive: true })
-  process.chdir(previousCwd)
-  console.log('mccli initialized!')
+	process.chdir(previousCwd)
+	console.log('mccli initialized!')
 }
