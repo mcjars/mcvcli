0ad9e1991a0421e7036a47a5a6a899fa8a02793a
0x7d8 <rjansengd@gmail.com>
2024-03-24 09:26:31 +0100

    improve eula accept

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -2,16 +2,36 @@ import fs from "fs"
 import cp from "child_process"
 import path from "path"
 import getConfig from "src/utils/config"
+import enquirer from "enquirer"
 
 export type Args = {}
 
-export default function start(args: Args) {
+export default async function start(args: Args) {
 	const config = getConfig()
+
+	const eula = await fs.promises.readFile('eula.txt', 'utf8').catch(() => '').then((eula) => eula.includes('eula=true'))
+
+	if (eula) {
+		console.log('eula already accepted!')
+	} else {
+		const { accept } = await enquirer.prompt<{
+			accept: boolean
+		}>({
+			type: 'confirm',
+			message: 'Do you accept the Minecraft EULA? (https://www.minecraft.net/eula)',
+			name: 'accept'
+		})
+
+		if (!accept) {
+			console.log('eula not accepted, server will not start!')
+			process.exit(1)
+		} else {
+			await fs.promises.writeFile('eula.txt', 'eula=true')
+		}
+	}
 
 	console.log('starting server...')
 	console.log(`java -Xmx${config.data.ramMB}M -jar ${config.data.jarFile} nogui`)
-
-	fs.writeFileSync('eula.txt', 'eula=true')
 
 	const child = cp.spawn('java', [`-Xmx${config.data.ramMB}M`, '-jar', config.data.jarFile, 'nogui'], {
 		cwd: path.dirname(config.data.jarFile)
