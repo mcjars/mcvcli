9839fb64a890079fe89dbac64004700ffe99e647
0x7d8 <rjansengd@gmail.com>
2024-03-24 18:15:21 +0100

    typos

--- src/commands/cache/clear.ts
+++ src/commands/cache/clear.ts
@@ -1,3 +1,4 @@
+import bytes from "bytes"
 import chalk from "chalk"
 import getCache from "src/utils/cache"
 
@@ -7,9 +8,14 @@ export default async function cacheClear(args: Args) {
 	const cache = getCache(),
 		size = cache.size()
 
+	if (!size) {
+		console.log('cache is already empty!')
+		process.exit(1)
+	}
+
 	for (const key of cache.keys()) {
 		cache.delete(key)
 	}
 
-	console.log('cleared cache!', chalk.cyan(`-${size}`))
+	console.log('cleared cache!', chalk.cyan(`-${bytes(size)}`))
 }
